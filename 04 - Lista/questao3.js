bc88dcf12dcf33b1adf91ae9567e1b6ee36de034
M. Fernanda <n.nandcchi@gmail.com>
2025-10-06 08:20:38 -0300

    Delete 04 - Lista directory

--- 04 - Lista/questao3.js
+++
@@ -1,7 +0,0 @@
-// Conversão de temperaturas (Celsius para Fahrenheit)
-
-console. log ("Convertor de temperatura: Celsius -> Fahrenheit");
-let temperatura_celsius = prompt ("Temperatura em Celsius:"); // Usuário informa a temperatura, em Celsius 
-
-let temperatura_fahrenheit = ((temperatura_celsius * 9/5) + 32); // Cálculo para conversão de temperatura
-console.log ("Temperatura em Fahrenheit: " + temperatura_fahrenheit); // Mostra a conversão feita
